062379fe8e9620de294d1e9bb9ba4568c0bf49aa
JohanVillazar <villazarjohan@gmail.com>
2025-10-27 12:28:18 -0500

    updatecors

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ import cors from "cors";
 import app from "./app.js";
 
 dotenv.config();
+//CORS
+app.use(cors({
+  origin: [
+    "https://magictravel.vercel.app", // URL frontend desplegado en Vercel
+    "http://localhost:5173"           // para entorno local (Vite/React)
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 mongoose.set("strictQuery", false);
 const PORT = process.env.PORT || 5000;
 const mongoURI = process.env.MONGO;
