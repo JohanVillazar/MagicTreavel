2387360934ae9b85a616c1d9a5b3c78b3cf6b708
JohanVillazar <villazarjohan@gmail.com>
2025-10-27 10:50:53 -0500

    updateroutes

--- routes/hotels.js
+++ routes/hotels.js
@@ -14,7 +14,7 @@ import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
 //CREATE
-router.post("/", verifyAdmin, createHotel);
+router.post("/create", verifyAdmin, createHotel);
 
 //UPDATE
 router.put("/:id", verifyAdmin, updateHotel);
